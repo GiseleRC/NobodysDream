5f8c8213be0b7eed5c714a7ce060fe71354be5a0
Gisele R. Caamaño <88439393+GiseleRC@users.noreply.github.com>
2023-05-03 14:25:40 -0300

    add script

--- Assets/Scripts/PlaneScripts/EnableGhostPlane.cs
+++ Assets/Scripts/PlaneScripts/EnableGhostPlane.cs
@@ -4,30 +4,5 @@ using UnityEngine;
 
 public class EnableGhostPlane : MonoBehaviour
 {
-    public GameState gameState;
-    public GhostCloth ghostCloth;
-    public GameObject glasses, map;
-    [SerializeField] private Collider ghostC, glassesC, mapC;
-    void Update()
-    {
-        
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other == ghostC)
-        {
-            ghostCloth.collitionE = true;
-            gameState.GhostPlaneModeEnabled = true;
-        }
-        if (other == glassesC)
-        {
-            glasses.SetActive(false);
-            map.SetActive(true);
-        }
-        if (other == mapC)
-        {
-            map.SetActive(false);
-            gameState.DemonPlaneModeEnabled = true;
-        }
-    }
+
 }
---
+++ Assets/Scripts/Player&children/PlayerCollitionsBody.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollitionsBody : MonoBehaviour
+{
+    //public GameState gameState;
+    //public GhostCloth glassesSC;
+    //public GameObject glasses, map;
+    //[SerializeField] private Collider glassesC, glassesC, mapC;
+    //void Update()
+    //{
+
+    //}
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other == ghostC)
+    //    {
+    //        ghostCloth.collitionE = true;
+    //        gameState.GhostPlaneModeEnabled = true;
+    //    }
+    //    if (other == glassesC)
+    //    {
+    //        glasses.SetActive(false);
+    //        map.SetActive(true);
+    //    }
+    //    if (other == mapC)
+    //    {
+    //        map.SetActive(false);
+    //        gameState.DemonPlaneModeEnabled = true;
+    //    }
+    //}
+}
