a9f39352ff2ab4b0599a58ae41e911b6d12af7dd
Gisele R. Caamaño <88439393+GiseleRC@users.noreply.github.com>
2023-05-03 14:37:33 -0300

    add script
    add script player colliders body

--- Assets/Scripts/Player&children/PlayerCollitionsBody.cs
+++ Assets/Scripts/Player&children/PlayerCollitionsBody.cs
@@ -4,30 +4,26 @@ using UnityEngine;
 
 public class PlayerCollitionsBody : MonoBehaviour
 {
-    //public GameState gameState;
-    //public GhostCloth glassesSC;
-    //public GameObject glasses, map;
-    //[SerializeField] private Collider glassesC, glassesC, mapC;
-    //void Update()
-    //{
-
-    //}
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other == ghostC)
-    //    {
-    //        ghostCloth.collitionE = true;
-    //        gameState.GhostPlaneModeEnabled = true;
-    //    }
-    //    if (other == glassesC)
-    //    {
-    //        glasses.SetActive(false);
-    //        map.SetActive(true);
-    //    }
-    //    if (other == mapC)
-    //    {
-    //        map.SetActive(false);
-    //        gameState.DemonPlaneModeEnabled = true;
-    //    }
-    //}
+    public GameState gameState;
+    public GhostCloth glassesSC;
+    public GameObject glasses, map;
+    [SerializeField] private Collider glassesC, mapC;
+    void Update()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == glassesC)
+        {
+            glassesSC.collitionE = true;
+            gameState.GhostPlaneModeEnabled = true;
+            glasses.SetActive(false);
+            map.SetActive(true);
+        }
+        if (other == mapC)
+        {
+            map.SetActive(false);
+            gameState.DemonPlaneModeEnabled = true;
+        }
+    }
 }
