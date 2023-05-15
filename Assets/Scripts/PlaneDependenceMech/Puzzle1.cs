61a007d4862b99c1688880fc4c4d54fc75ae7408
GiseleRC <rominagisele.ar@gmail.com>
2023-05-15 13:55:07 -0300

    commit

--- Assets/Scripts/PlaneDependenceMech/Puzzle1.cs
+++ Assets/Scripts/PlaneDependenceMech/Puzzle1.cs
@@ -5,7 +5,35 @@ using UnityEngine;
 public class Puzzle1 : MonoBehaviour
 {
     public GameObject textHere, sensorplayerRigth, sensorIncorrect;
-  
+    bool stairPart1 = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Cube")
+        {
+            stairPart1 = true;
+            Debug.Log(stairPart1);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            stairPart1 = true;
+            Debug.Log(stairPart1 + "Player");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Cube")
+        {
+            stairPart1 = false;
+            Debug.Log(stairPart1);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            stairPart1 = false;
+            Debug.Log(stairPart1 + "Player");
+        }
+    }
     private void Update()
     {
         if (textHere)
