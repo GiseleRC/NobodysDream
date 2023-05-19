af5909e097da5a8f110e329da2bd2ce3101f1c8f
GiseleRC <rominagisele.ar@gmail.com>
2023-05-19 17:39:13 -0300

    comit

--- Assets/Scripts/Dianas.cs
+++ Assets/Scripts/Dianas.cs
@@ -6,6 +6,7 @@ public class Dianas : MonoBehaviour
 {
     public Puzzle2 puzzle2;
     public GameObject pivotBook;
+    public GameObject particle1, particle2, particle3;
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Ball(Clone)")
@@ -13,17 +14,17 @@ public class Dianas : MonoBehaviour
             if (gameObject.name == "Diana1")
             {
                 puzzle2.doorEnable1 = true;
-                Debug.Log("1" + puzzle2.doorEnable1);
+
             }
             else if (gameObject.name == "Diana2")
             {
                 puzzle2.doorEnable2 = true;
-                Debug.Log("2" + puzzle2.doorEnable2);
+
             }
             else if (gameObject.name == "Diana3")
             {
                 puzzle2.doorEnable3 = true;
-                Debug.Log("3" + puzzle2.doorEnable3);
+
             }
 
         }
