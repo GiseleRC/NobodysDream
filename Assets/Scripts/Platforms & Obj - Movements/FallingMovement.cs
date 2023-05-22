4b9fadc77fd187921d36c93c18cfd90832be9ee5
Escler <lean07955@gmail.com>
2023-05-22 18:51:19 -0300

    Timer en plataformas que caen

--- Assets/Scripts/Platforms & Obj - Movements/FallingMovement.cs
+++ Assets/Scripts/Platforms & Obj - Movements/FallingMovement.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class FallingMovement : MonoBehaviour
 {
-    bool isFalling = false;
-    float fallingVelocity = 0;
+    bool isFalling = false, startTimer;
+    float fallingVelocity = 0, currentTimer;
+    [SerializeField] float timeToFall;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Char")
         {
-            isFalling = true;
+            startTimer = true;
         }
     }
+
     void Update()
     {
-        if (isFalling)
+        if (startTimer)
+        {
+            GetComponent<PlatformMove>().enabled = true;
+            currentTimer += Time.deltaTime;
+        }
+
+        if (currentTimer > timeToFall)
         {
             fallingVelocity += Time.deltaTime / 10;
             gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - fallingVelocity, gameObject.transform.position.z);
         }
         if (gameObject.transform.position.y <= -40)
         {
-            isFalling = false;
+            startTimer = false;
+            GetComponent<PlatformMove>().enabled = false;
+            currentTimer = 0;
             fallingVelocity = 0;
         }
     }
