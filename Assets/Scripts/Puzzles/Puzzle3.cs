01ea210d2f2b1d9f1cfdb603f032b4856ac26dee
GiseleRC <rominagisele.ar@gmail.com>
2023-05-26 17:17:33 -0300

    rotate faces

--- Assets/Scripts/Puzzles/Puzzle3.cs
+++ Assets/Scripts/Puzzles/Puzzle3.cs
@@ -7,22 +7,25 @@ public class Puzzle3 : MonoBehaviour
     public bool showGuess1 = false;
     public bool showGuess2 = false;
     public bool showGuess3 = false;
-    public GameObject guess1GO, guess2GO, guess3GO;
+    public GameObject guess1GO, guess2GO, guess3GO, face1GO, face2GO, face3GO;
 
     private void Update()
     {
         if (showGuess1)
         {
+            face1GO.transform.Rotate(0, 180, 0);
             guess1GO.SetActive(showGuess1);
             showGuess1 = false;
         }
         if (showGuess2)
         {
+            face2GO.transform.Rotate(0, 180, 0);
             guess2GO.SetActive(showGuess2);
             showGuess2 = false;
         }
         if (showGuess1)
         {
+            face3GO.transform.Rotate(0, 180, 0);
             guess3GO.SetActive(showGuess3);
             showGuess3 = false;
         }
