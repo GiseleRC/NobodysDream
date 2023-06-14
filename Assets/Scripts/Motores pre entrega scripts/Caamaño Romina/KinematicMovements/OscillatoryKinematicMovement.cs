e2a8b582668d9c41c7285f85e37b28c1c509a6b0
Gisele R. Caamaño <88439393+GiseleRC@users.noreply.github.com>
2023-06-14 12:56:59 -0300

    comentarios

--- Assets/Scripts/Motores pre entrega scripts/Caamaño Romina/KinematicMovements/OscillatoryKinematicMovement.cs
+++ Assets/Scripts/Motores pre entrega scripts/Caamaño Romina/KinematicMovements/OscillatoryKinematicMovement.cs
@@ -9,27 +9,22 @@ public class OscillatoryKinematicMovement : KinematicMovement
     [SerializeField] protected Vector3 direction = Vector3.right;
     [SerializeField] protected float amplitude = 5f;
     [SerializeField] protected float frequency = 0.15f;
-
     protected float t = 0f;
-    protected Vector3 origin;
 
-    //TP2 - Caamaño Romina - Utiliza la funcion de reset en que no hace nada, 
-    protected virtual void Start()
-    {
-        ResetMovement();
-    }
-    //TP2 - Caamaño Romina - sobreescribe el metodo llamado en el awake que no hace nada originalmente
+    //TP2 - Caamaño Romina - sobreescribe el metodo llamado que no hace nada originalmente
     public override void ResetMovement()
     {
         t = 0f;
-        origin = transform.position;
     }
     //TP2 - Caamaño Romina - Utiliza los metodos heredados y los sobreescribe
     public override Vector3 GetPositionDelta(float dt)
     {
+        //calcula el moviemitno anterior
         Vector3 prev = direction.normalized * amplitude * Mathf.Sin(2f * Mathf.PI * frequency * t);
-        t += dt;
+        t += dt;//se actializa el tiempo
+        //calcula el moviento con el tiempo actualizadop
         Vector3 curr = direction.normalized * amplitude * Mathf.Sin(2f * Mathf.PI * frequency * t);
+        //devuelve la diferencia de moviemintos
         return curr - prev;
     }
 
