6c8dd0ac0c96354a03f3f6ada2045b8d6ad26bfd
Escler <lean07955@gmail.com>
2023-06-14 19:19:31 -0300

    View Bobbing

--- Assets/Scripts/Camera/MoveCamera.cs
+++ Assets/Scripts/Camera/MoveCamera.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     Transform cameraPos;
+    float timer, defaultPosY, defaultPosX;
+    [SerializeField] float boobingSpeed, bobbingAmount;
     // Start is called before the first frame update
 
     private void Awake()
@@ -20,12 +22,31 @@ public class MoveCamera : MonoBehaviour
     void Start()
     {
         cameraPos = GameObject.Find("cameraPos").GetComponent<Transform>();
+        defaultPosY = cameraPos.transform.localPosition.y;
+        defaultPosX = cameraPos.transform.localPosition.x;
+        transform.position = cameraPos.transform.position;
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        transform.position = cameraPos.position;
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+
+        if (inputX != 0 || inputY != 0)
+        {
+            timer += Time.deltaTime * boobingSpeed;
+            cameraPos.transform.localPosition = new Vector3(cameraPos.transform.localPosition.x + Mathf.Cos(timer) * bobbingAmount * Time.deltaTime, cameraPos.transform.localPosition.y + Mathf.Cos(timer) * bobbingAmount * Time.deltaTime, cameraPos.transform.localPosition.z);
+        }
+        else
+        {
+            cameraPos.transform.localPosition = new Vector3(Mathf.Lerp(cameraPos.transform.localPosition.x, defaultPosX, Time.deltaTime * boobingSpeed), Mathf.Lerp(cameraPos.transform.localPosition.y, defaultPosY, Time.deltaTime * boobingSpeed), cameraPos.localPosition.z);
+
+        }
+
+        transform.position = cameraPos.transform.position;
     }
 
     private void OnPauseStateChanged(PauseState newPauseState)
