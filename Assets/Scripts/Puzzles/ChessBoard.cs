d01ae6ecb147726636b14577a44aa477430b007e
Mati Lozada <matias.lozada@nimblegiant.com>
2023-06-20 22:48:18 -0300

    COMMIT

--- Assets/Scripts/Puzzles/ChessBoard.cs
+++ Assets/Scripts/Puzzles/ChessBoard.cs
@@ -6,13 +6,6 @@ public class ChessBoard : MonoBehaviour
 {
     [SerializeField] public ChessPiece[] chessPieces;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        //if (collision.gameObject.name == "Char")
-        //{
-
-        //}
-    }
     public void ResetBoard()
     {
         foreach (ChessPiece chessPiece in chessPieces)
--- Assets/Scripts/Puzzles/ChessPiece.cs
+++ Assets/Scripts/Puzzles/ChessPiece.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ChessPiece : MonoBehaviour
 {
     private Vector3 initialPos;
+    private GameObject piece1Bool, piece2Bool, piece3Bool, piece4Bool, piece5Bool, piece6Bool;
 
     private void Start()
     {
@@ -15,5 +16,41 @@ public class ChessPiece : MonoBehaviour
     {
         gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         transform.localPosition = initialPos;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(gameObject.name == "Peon 7-4" && other.name == "Casillero  7-2")
+        {
+            piece1Bool.SetActive(true);
+        }
+        else if (gameObject.name == "Caballo 8-3" && other.name == "Casillero  7-1")
+        {
+            piece2Bool.SetActive(true);
+        }
+        else if (gameObject.name == "Alfil 4-3" && other.name == "Casillero  6-1")
+        {
+            piece3Bool.SetActive(true);
+        }
+        else if (gameObject.name == "Peon 5-3" && other.name == "Casillero  5-2")
+        {
+            piece4Bool.SetActive(true);
+        }
+        else if (gameObject.name == "Peon 2-3" && other.name == "Casillero 1-2")
+        {
+            piece5Bool.SetActive(true);
+        }
+        else if (gameObject.name == "Torre 1-4" && other.name == "Casillero  1-1")
+        {
+            piece6Bool.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (piece1Bool && piece2Bool && piece3Bool && piece4Bool && piece5Bool && piece6Bool)
+        {
+
+        }
     }
 }
