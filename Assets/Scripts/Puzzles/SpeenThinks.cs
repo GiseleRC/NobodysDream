11901ba5796ad87260cc65f3d9010048bfcc6b62
GiseleRC <rominagisele.ar@gmail.com>
2023-06-22 02:02:10 -0300

    oreos con transicion

--- Assets/Scripts/Puzzles/SpeenThinks.cs
+++ Assets/Scripts/Puzzles/SpeenThinks.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class SpeenThinks : MonoBehaviour
 {
-    public float rotX, rotY, rotZ;
+    public float rotX, rotY, rotZ, currRotX;
+    private bool oreoStartRot = false;
     public GameObject partTwoOflevel2, particle;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name == "Ball(Clone)")
+        if (other.name == "Ball(Clone)" && gameObject.name == "Pizarron")
         {
             gameObject.transform.Rotate(rotX, rotY, rotZ);
-
-            if (gameObject.name == "Pizarron")
-            {
-                partTwoOflevel2.SetActive(true);
-                particle.SetActive(false);
-            }
+            partTwoOflevel2.SetActive(true);
+            particle.SetActive(false);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Ball(Clone)")
+        if (collision.gameObject.name == "Ball(Clone)" && gameObject.name == "OreoL")
         {
-            gameObject.transform.Rotate(rotX, rotY, rotZ);
+            oreoStartRot = true;
+            gameObject.GetComponent<KinematicMovementController>().enabled = true;
         }
+    }
+
+    private void Update()
+    {
+        if (gameObject.name == "OreoL" && oreoStartRot)
+        {
+            currRotX = gameObject.transform.rotation.x;
+            if (currRotX >= 1f)
+            {
+                gameObject.GetComponent<KinematicMovementController>().enabled = false;
+                oreoStartRot = false;
+            }
+        }
+
     }
 }
