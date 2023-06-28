ce9526274f538e995b23f4d4e762ff130a8e4189
GiseleRC <rominagisele.ar@gmail.com>
2023-06-28 18:58:50 -0300

    particula de cofre

---
+++ Assets/Scripts/Platforms & Obj - Movements/ChestPortal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestPortal : MonoBehaviour
+{
+    public GameObject particlePortal;
+
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "KakiPeluche")
+        {
+            collision.gameObject.SetActive(false);
+            particlePortal.SetActive(true);
+        }
+    }
+}
