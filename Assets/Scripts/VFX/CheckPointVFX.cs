e59f31b8194a8504063ab3a06abc8e2981c21a3a
GiseleRC <rominagisele.ar@gmail.com>
2023-06-30 15:38:01 -0300

    commit

--- Assets/Scripts/VFX/CheckPointVFX.cs
+++ Assets/Scripts/VFX/CheckPointVFX.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 public class CheckPointVFX : MonoBehaviour
 {
     [SerializeField] GameObject torch;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
        torch.GetComponent<EnableFireTorch>().PlayFire();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+       gameObject.GetComponent<Collider>().enabled = false;
     }
 }
