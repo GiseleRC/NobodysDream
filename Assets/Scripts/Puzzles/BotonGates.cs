1946f5d483fc44562f85fd8dde35a4c9bce6e756
GiseleRC <rominagisele.ar@gmail.com>
2023-07-02 13:06:35 -0300

    minijefe finished

--- Assets/Scripts/Puzzles/BotonGates.cs
+++ Assets/Scripts/Puzzles/BotonGates.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BotonGates : MonoBehaviour
 {
     [SerializeField] private MiniBoss miniBoss;
+    [SerializeField] public GameObject ball2GO;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Ball(Clone)" && gameObject.name == "ResetGreen")
@@ -14,10 +16,12 @@ public class BotonGates : MonoBehaviour
         else if (collision.gameObject.name == "Ball(Clone)" && gameObject.name == "ResetBlue")
         {
             miniBoss.gateBlueOpen = true;
+            ball2GO.SetActive(true);
         }
         else if (collision.gameObject.name == "Ball(Clone)" && gameObject.name == "ResetPink")
         {
             miniBoss.gatePinkOpen = true;
+            ball2GO.SetActive(true);
         }
     }
 
--- Assets/Scripts/Puzzles/SensorBall.cs
+++ Assets/Scripts/Puzzles/SensorBall.cs
@@ -16,36 +16,42 @@ public class SensorBall : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (gameObject.name == "SensorGlasses" && collision.gameObject.name == "BowlinBall - primera")
+        if (gameObject.name == "BowlinBall - primera" && collision.gameObject.name == "SensorGlasses")
         {
             Z1GO.SetActive(false);
-            miniBoss.gateGreenClose = true;
-            collision.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            
+            //miniBoss.gateGreenClose = false;
         }
-        else if (gameObject.name == "SensorGlasses" && collision.gameObject.name == "BowlinBall - segunda")
+        else if (gameObject.name == "BowlinBall - segunda" && collision.gameObject.name == "SensorGlasses" )
         {
-            miniBoss.gateGreenClose = true;
             Z2GO.SetActive(false);
             cartelGO.SetActive(true);
             gameObject.SetActive(false);
             collision.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            //miniBoss.gateGreenClose = false;
         }
 
-        if (gameObject.name == "BowlinBall - primera" || gameObject.name == "BowlinBall - segunda" && collision.gameObject.name == "Compuerta tobogan")
+        if (gameObject.name == "BowlinBall - segunda" && collision.gameObject.name == "Compuerta tobogan")
         {
             miniBoss.gateBlueClose = true;
             miniBoss.gatePinkClose = true;
+            //miniBoss.gateBlueOpen = false;
+            //miniBoss.gatePinkOpen = false;
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.name == "BowlinBall - primera" && other.gameObject.name == "SensorClose" )
+        {
+            miniBoss.gateGreenClose = true;
+            //miniBoss.gateGreenOpen = false;
+        }
+        else if (gameObject.name == "BowlinBall - segunda" && other.gameObject.name == "SensorClose")
+        {
+            miniBoss.gateGreenClose = true;
+            //miniBoss.gateGreenOpen = false;
         }
     }
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (gameObject.name == "SensorClose" && other.gameObject.name == "BowlinBall - primera")
-    //    {
-            
-    //    }
-    //    else if (gameObject.name == "SensorClose" && other.gameObject.name == "BowlinBall - segunda")
-    //    {
-    //        miniBoss.gateGreenClose = true;
-    //    }
-    //}
 }
