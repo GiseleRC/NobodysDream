f2dbb43cbad2b83ca8223d79a2d1b2df5cb02fed
GiseleRC <rominagisele.ar@gmail.com>
2023-07-02 20:37:26 -0300

    COMMIT

---
+++ Assets/Scripts/Puzzles/DetectionEvents.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionEvents : MonoBehaviour
+{
+    [SerializeField] private FinalPuzzle finalPuzzle;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.name == "Char")
+        {
+
+        }
+
+        if (gameObject.name == "lightsOn")
+        {
+
+        }
+
+        if (gameObject.name == "SlimeFloor1")
+        {
+
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Puzzles/FinalPuzzle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalPuzzle : MonoBehaviour
+{
+    [SerializeField] GameObject Medio1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
