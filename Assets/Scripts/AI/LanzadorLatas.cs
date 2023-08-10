64425777ee474fac08af518e11d930174458e324
Escler <lean07955@gmail.com>
2023-08-09 23:59:37 -0300

    Lanzador de lata + Lata, Ventilador ajustado

---
+++ Assets/Scripts/AI/LanzadorLatas.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanzadorLatas : MonoBehaviour
+{
+    public GameObject lata, bloqueo, spawnPos;
+    public float cdThrow, waitingTime;
+    float timer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instantiate(lata, spawnPos.transform.position, spawnPos.transform.rotation);
+        timer = cdThrow;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer < 0)
+        {
+            bloqueo.SetActive(false);
+            waitingTime -= Time.deltaTime;
+            if(waitingTime < 0)
+            {
+                bloqueo.SetActive(true);
+                timer = cdThrow;
+                Instantiate(lata, spawnPos.transform.position, spawnPos.transform.rotation);
+                waitingTime = 4f;
+            }
+        }
+    }
+}
