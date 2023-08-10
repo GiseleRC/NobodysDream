c37f7938ec36005cb700e39fb7569468f8b7f516
GiseleRC <rominagisele.ar@gmail.com>
2023-08-10 18:48:16 -0300

    commit

--- Assets/Scripts/Puzzles/PuzzleBlueSquare/GhostMovement.cs
+++ Assets/Scripts/Puzzles/PuzzleBlueSquare/GhostMovement.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class GhostMovement : MonoBehaviour
 {
     [SerializeField] public bool canITalk;
-    //[SerializeField] private GameObject dialog;
+    [SerializeField] private GameObject dialog, disablesensor;
+    [SerializeField] public float timeCount;
+
     void Update()
     {
+
         if (canITalk)
         {
-            //dialog.SetActive(true);
+            timeCount -= Time.deltaTime;
+            canITalk = false;
         }
+        if (timeCount <= 0f)
+        {
+            dialog.SetActive(false);
+            //HACER QUE EL FANTASMA AL TERMINAR EL TIEMPO RETROCEDA HACIA ATRAS DE TABLERO
+        }
+    }
+
+    public void CanTalk()
+    {
+        dialog.SetActive(true);
+        canITalk = true;
+        disablesensor.SetActive(false);
     }
 }
