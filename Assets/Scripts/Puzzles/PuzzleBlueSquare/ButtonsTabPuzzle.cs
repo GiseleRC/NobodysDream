578295c8a713aa9ac1b1491a32ba3afa364b6ec8
GiseleRC <rominagisele.ar@gmail.com>
2023-08-10 16:04:53 -0300

    puzzle de puerta

--- Assets/Scripts/Puzzles/PuzzleBlueSquare/ButtonsTabPuzzle.cs
+++ Assets/Scripts/Puzzles/PuzzleBlueSquare/ButtonsTabPuzzle.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class ButtonsTabPuzzle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private PuzzleBlueSquareTab puzzleBlueSquareTab;
+    [SerializeField] public GameObject lightCorrButton1, lightCorrButton2, lightCorrButton3, lightCorrButton4;
+    [SerializeField] public GameObject lightInButton1, lightInButton2;
+    //declarar un audio clip
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.name == "Ball(Clone)")
+        {
+            //reproducir un sonido de click
+
+            if (gameObject.name == "Correct1")
+            {
+                puzzleBlueSquareTab.correctButton1 = true;
+                lightCorrButton1.SetActive(true);
+            }
+            else if (gameObject.name == "Correct2")
+            {
+                puzzleBlueSquareTab.correctButton2 = true;
+                lightCorrButton2.SetActive(true);
+            }
+            else if (gameObject.name == "Correct3")
+            {
+                puzzleBlueSquareTab.correctButton3 = true;
+                lightCorrButton3.SetActive(true);
+            }
+            else if (gameObject.name == "Correct4")
+            {
+                puzzleBlueSquareTab.correctButton3 = true;
+                lightCorrButton4.SetActive(true);
+            }
+
+            if (gameObject.name == "Incorrect1")
+            {
+                puzzleBlueSquareTab.incorrectButton1 = true;
+                lightInButton1.SetActive(true);
+            }
+            else if (gameObject.name == "Incorrect2")
+            {
+                puzzleBlueSquareTab.incorrectButton2 = true;
+                lightInButton2.SetActive(true);
+            }
+        }
     }
 }
--- Assets/Scripts/Puzzles/PuzzleBlueSquare/PuzzleBlueSquareTab.cs
+++ Assets/Scripts/Puzzles/PuzzleBlueSquare/PuzzleBlueSquareTab.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PuzzleBlueSquareTab : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] public bool correctButton1, correctButton2, correctButton3, correctButton4;
+    [SerializeField] public bool incorrectButton1, incorrectButton2;
+    [SerializeField] private GameObject[] buttonsLightDisable;
+    [SerializeField] private GameObject doorCollider, lightOfDoorgreen, lightOfDoorred;
+    //declarar un sonido de incorrecto
 
-    // Update is called once per frame
+    public void DoorCollider(bool enableDoor)
+    {
+        doorCollider.GetComponent<Collider>().enabled = enableDoor;
+    }
     void Update()
     {
-        
+        if (correctButton1 && correctButton2 && correctButton3 && correctButton4)
+        {
+            if (incorrectButton1 || incorrectButton2)
+            {
+                //hacer un sonido de incorrecto
+                //
+                correctButton1 = false;
+                correctButton2 = false;
+                correctButton3 = false;
+                correctButton4 = false;
+                incorrectButton1 = false;
+                incorrectButton2 = false;
+
+                for (int i = 0; i < buttonsLightDisable.Length; i++)
+                {
+                    buttonsLightDisable[i].SetActive(false);
+                }
+            }
+            else
+            {
+                //hacer un sonido de correcto
+                //
+                correctButton1 = false;
+                correctButton2 = false;
+                correctButton3 = false;
+                correctButton4 = false;
+                lightOfDoorgreen.SetActive(true);
+                lightOfDoorred.SetActive(false);
+
+                DoorCollider(true);
+            }
+
+        }
     }
 }
