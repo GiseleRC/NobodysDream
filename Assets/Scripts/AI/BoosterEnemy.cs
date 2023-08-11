632ea2289d23fb24c8ecb90c243948f6c6c3e5cf
Escler <lean07955@gmail.com>
2023-08-11 05:44:09 -0300

    Subo sonido ZzZ

--- Assets/Scripts/AI/BoosterEnemy.cs
+++ Assets/Scripts/AI/BoosterEnemy.cs
@@ -16,6 +16,7 @@ public class BoosterEnemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GameObject.Find("PlayRandomAudios").GetComponent<AudioSource>();
         meshRenderer = gameObject.GetComponentInChildren<MeshRenderer>();
         meshRenderer.material = blue;
         actualTimer = timer;
