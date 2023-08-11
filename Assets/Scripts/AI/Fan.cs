2b5359f18ed48c1b9cb6f7b21e3666c1c5b12a47
Escler <lean07955@gmail.com>
2023-08-11 01:35:07 -0300

    Cambios

--- Assets/Scripts/AI/Fan.cs
+++ Assets/Scripts/AI/Fan.cs
@@ -10,7 +10,7 @@ public class Fan : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        actualTimer = timer;
+        actualTimer = timer / 2;
     }
 
     // Update is called once per frame
